acb4c5b3d94979a29ec94b28d70149703a1b1414
HarIgo23 <igorn2304@gmail.com>
2020-02-08 21:18:18 +0300

    Create model movie_reviews

--- movie_reviews/models.py
+++ movie_reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from movies.models import Movie
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class MovieReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    score = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    text = models.TextField(null=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
