1ed166b7998bff63455dbaf0f45dfc646cc8f996
HarIgo23 <igorn2304@gmail.com>
2020-02-10 18:04:25 +0300

    add standart login and register

--- social_auth/views.py
+++ social_auth/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def login_page(request):
@@ -9,3 +11,17 @@ def login_page(request):
 @login_required
 def profile_page(request):
     return render(request, 'social_auth/profile.html')
+
+
+def register_page(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'Account created for {username}!')
+            return redirect('profile')
+    else:
+        form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "social_auth/register.html", context)
