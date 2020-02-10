4f69e5c7af8205d9af139727aae5ba9cce1988db
HarIgo23 <igorn2304@gmail.com>
2020-02-10 21:33:35 +0300

    add creating review in profile

--- social_auth/views.py
+++ social_auth/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from movie_reviews.forms import MovieReviewModelForm
+from movie_reviews.models import MovieReview
 
 
 def login_page(request):
@@ -10,7 +12,17 @@ def login_page(request):
 
 @login_required
 def profile_page(request):
-    return render(request, 'social_auth/profile.html')
+    form = MovieReviewModelForm(request.POST or None)
+    user = request.user
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.user = user
+        obj.save()
+        form = MovieReviewModelForm()
+        messages.success(request, "Review added successful")
+    reviews = user.moviereview_set.all()
+    context = {'form_review': form, 'reviews': reviews}
+    return render(request, 'social_auth/profile.html', context)
 
 
 def register_page(request):
